2f9dbdb9872216f51651ca2e7d9bb2c38f9b8fa4
Chris <christianotu.d@gmail.com>
2021-01-05 13:08:46 +0100

    Added search redux slice logic including thunk

--- src/slices/SearchSlice.ts
+++ src/slices/SearchSlice.ts
@@ -1,14 +1,61 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { State, TodoItem } from "../types";
+import { searchMovies } from "../api/omdbApi";
+import { Search, Movie, AppThunk } from "../types";
 
-const initialState = {};
+const initialState: Search = {
+  id: [],
+  movies: {},
+  loading: false,
+  error: null,
+};
 
 const search = createSlice({
   name: "search",
   initialState,
-  reducers: {},
+  reducers: {
+    searchMoviesStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+
+    searchMoviesSuccess(state, { payload }: PayloadAction<Array<Movie>>) {
+      payload.forEach((movie) => {
+        const { imdbID } = movie;
+
+        state.id.push(imdbID);
+        state.movies[imdbID] = movie;
+      });
+
+      state.loading = false;
+      state.error = null;
+    },
+
+    searchMoviesFailure(state, { payload }: PayloadAction<string>) {
+      state.error = payload;
+      state.loading = false;
+    },
+  },
 });
 
-//export const { deleteItem, addItem } = search.actions;
+export const {
+  searchMoviesStart,
+  searchMoviesSuccess,
+  searchMoviesFailure,
+} = search.actions;
 
-export default search.reducer;
+export default search.reducer; 
+
+//Redux Thunk
+export const fetchMovies = (movieTitle: string): AppThunk => async (
+  dispatch
+) => {
+  try {
+    dispatch(searchMoviesStart());
+
+    const movies = await searchMovies(movieTitle);
+
+    dispatch(searchMoviesSuccess(movies));
+  } catch (error) {
+    dispatch(searchMoviesFailure(error.message));
+  }
+};
