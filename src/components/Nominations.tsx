8f9c2555ae33203ba128badd442d1abc4cd13109
Chris <christianotu.d@gmail.com>
2021-01-06 13:45:14 +0100

    Updated nomination component

--- src/components/Nominations.tsx
+++ src/components/Nominations.tsx
@@ -1,9 +1,61 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getNominations } from "../selectors";
+import { deleteNomination } from "../slices/nominationsSlice";
+import { Movie } from "../types";
+import { getStoredNominations } from "../utils";
 
 export const Nominations: React.FC = () => {
-    return (
-        <div>
-            <p>Nominations</p>
-        </div>
-    );
-}
+  const { movies } = useSelector(getNominations);
+
+  const previousNominations = getStoredNominations(); //Get previously stored nominations
+
+  //Use either previously stored nominations or new ones
+  const nominations =
+    previousNominations.length !== 0
+      ? previousNominations
+      : Object.values(movies);
+
+  const dispatch = useDispatch();
+
+  const handleRemove = (id: string) => {
+    dispatch(deleteNomination(id));
+
+    window.localStorage.removeItem(id); //Remove nomination to local storage
+  };
+
+  return (
+    <aside aria-labelledby="nominations">
+      {nominations.length !== 0 && (
+        <h2 id="nominations" tabIndex={-1}>
+          Nominations
+        </h2>
+      )}
+
+      <div className="empty-state">
+        <p>
+          <strong>Your nominations appear here.</strong> <br />
+          <span>Nominations are saved even after leaving the browser</span>
+        </p>
+      </div>
+
+      <ul>
+        {nominations.map((movie: Movie) => (
+          <li key={movie.imdbID}>
+            <p className="text">
+              {movie.Title} ({movie.Year})
+            </p>
+            <button
+              type="button"
+              aria-label={`Delete ${movie.Title}`}
+              onClick={() => handleRemove(movie.imdbID)}
+            >
+              <span aria-hidden="true">Remove</span>
+              <span className="vh">Remove {`${movie.Title}`}</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
