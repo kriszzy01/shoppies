e12593ee638a42eadfa8142b0dc8d5498c885f9e
Chris <christianotu.d@gmail.com>
2021-01-06 13:44:16 +0100

    Updated for pagination

--- src/api/omdbApi.ts
+++ src/api/omdbApi.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export const searchMovies = async (movieTitle: string) => {
+export const searchMovies = async (movieTitle: string, page: number) => {
   const apikey = process.env.REACT_APP_API_KEY;
 
-  const requestURL = `http://www.omdbapi.com/?s=${movieTitle}&apikey=${apikey}`;
+  const requestURL = `http://www.omdbapi.com/?s=${movieTitle}&page=${page}&apikey=${apikey}`;
 
   try {
     const searchResults = await axios.get(requestURL);
 
-    const { Search } = searchResults.data;
+    const { Search, totalResults } = searchResults.data;
 
-    return Search;
+    return { Search, totalResults };
   } catch (error) {
     throw error;
   }
